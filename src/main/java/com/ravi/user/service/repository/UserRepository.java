19767245769ffc898f305fe14ef734b7cd4dcb01
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2023-02-07 02:00:51 +0530

    Delete src directory

--- src/main/java/com/ravi/user/service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ravi.user.service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ravi.user.service.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
